33ba72c6c9534549ccafd37c6eb3ce4544759b0d
Yann Le Merrer <yann@air-de-pascal.home>
2025-06-22 16:42:21 +0200

    Insert sessions into database

--- www/init_db.py
+++ www/init_db.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+import json
 
 connection = psycopg2.connect(
     host="localhost",
@@ -11,12 +12,25 @@ connection = psycopg2.connect(
 cursor = connection.cursor()
 
 cursor.execute('DROP TABLE IF EXISTS sessions;')
-cursor.execute("""CREATE TABLE sessions(id int NOT NULL PRIMARY KEY,
-                                        title VARCHAR(150) NOT NULL,
-                                        starts_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                                        ends_at TIMESTAMP WITH TIME ZONE NOT NULL) """)
+cursor.execute("""
+                CREATE TABLE sessions(id int NOT NULL PRIMARY KEY,
+                title VARCHAR(150) NOT NULL,
+                starts_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                ends_at TIMESTAMP WITH TIME ZONE NOT NULL)
+                """)
 
+def escape(string):
+    return string.replace("'", " ")
 
+with open("www/data/all.json") as file:
+    data = json.load(file)
+    sessions = data[0]['sessions']
+    for session in sessions:
+        title = escape(session["title"])
+        cursor.execute(f"""
+        INSERT INTO sessions(id, title, starts_at, ends_at)
+        VALUES('{session["id"]}', '{title}', '{session["startsAt"]}', '{session["endsAt"]}')
+        """)
 
 connection.commit()
 
