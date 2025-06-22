670dc916b68b4c7b68949d16f8f43c5ba6310f34
Yann Le Merrer <yann@air-de-pascal.home>
2025-06-22 16:11:36 +0200

    Create sessions table

--- www/init_db.py
+++ www/init_db.py
@@ -11,6 +11,12 @@ connection = psycopg2.connect(
 cursor = connection.cursor()
 
 cursor.execute('DROP TABLE IF EXISTS sessions;')
+cursor.execute("""CREATE TABLE sessions(id int NOT NULL PRIMARY KEY,
+                                        title VARCHAR(150) NOT NULL,
+                                        starts_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                                        ends_at TIMESTAMP WITH TIME ZONE NOT NULL) """)
+
+
 
 connection.commit()
 
